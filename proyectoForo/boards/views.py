076260bdb056f5a09f2cd05ed255087ebbc5a51c
miguelferra <miguelferra4@gmail.com>
2021-01-08 15:08:41 -0700

    Admin config and bootstrap implementation

--- proyectoForo/boards/views.py
+++ proyectoForo/boards/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from .models import Board
 def home(request):
-    return HttpResponse(""" <!DOCTYPE html><html><body><h1>My First </h1><p>My first paragraph.</p></body></html> """)
+    boards = Board.objects.all()
+    boards_names = list()
+
+    return render(request, 'home.html', {'boards':boards})
 
 def adios(request):
     return HttpResponse('Bye')
