f24c58835c403022bd950c16283c6a4f18660ba3
miguelferra <miguelferra4@gmail.com>
2021-01-09 18:48:41 -0700

    New topic working

--- proyectoForo/proyectoForo/urls.py
+++ proyectoForo/proyectoForo/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path(r'', views.home, name='home'),
     path(r'admin/', admin.site.urls),
     re_path(r'boards/(?P<pk>\d+)/$', views.board_topics, name ='board_topics'),
+    re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
 ]
