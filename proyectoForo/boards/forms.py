327bd412d6b074d1d7417b7b4a580577390b346a
miguelferra <miguelferra4@gmail.com>
2021-01-12 19:38:00 -0700

    Topic posts view, post reply and number of post and topics

--- proyectoForo/boards/forms.py
+++ proyectoForo/boards/forms.py
@@ -1,10 +1,13 @@
 from django import forms
 from .models import Topic
+from django import forms
+from .models import Post
+
 
 class NewTopicForm(forms.ModelForm):
     subject = forms.CharField(widget=forms.TextInput(
-            attrs={'placeholder': 'Tittle in here bro!'}
-        ))
+        attrs={'placeholder': 'Tittle in here bro!'}
+    ))
     message = forms.CharField(
         widget=forms.Textarea(
             attrs={'rows': 5, 'placeholder': 'Write some stuff here!!!...'}
@@ -16,3 +19,16 @@ class NewTopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['subject', 'message']
+
+
+class PostForm(forms.ModelForm):
+    message = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'rows': 5, 'placeholder': 'Write your reply here!!!...'}
+        ),
+        max_length=4000,
+        help_text='The max length of the text is 4000.'
+    )
+    class Meta:
+        model = Post
+        fields = ['message', ]
--- proyectoForo/boards/models.py
+++ proyectoForo/boards/models.py
@@ -1,6 +1,7 @@
-
 from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.utils.text import Truncator
 
 
 class Board(models.Model):
@@ -10,18 +11,32 @@ class Board(models.Model):
     def __str__(self):
         return self.name
 
+    def get_posts_count(self):
+        return Post.objects.filter(topic__board=self).count()
+
+    def get_last_post(self):
+        return Post.objects.filter(topic__board=self).order_by('-created_at').first()
+
 
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
-    board = models.ForeignKey(Board, on_delete= models.CASCADE, related_name='topics')
-    starter = models.ForeignKey(User,on_delete= models.CASCADE, related_name='topics')
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='topics')
+    starter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topics')
+    views = models.PositiveIntegerField(default=0)  # <- here
+
+    def __str__(self):
+        return self.subject
 
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
-    topic = models.ForeignKey(Topic,on_delete= models.CASCADE, related_name='posts')
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='posts')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
-    created_by = models.ForeignKey(User,on_delete= models.CASCADE, related_name='posts')
-    updated_by = models.ForeignKey(User,on_delete= models.CASCADE, null=True, related_name='+')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='+')
+
+    def __str__(self):
+        truncated_message = Truncator(self.message)
+        return truncated_message.chars(30)
